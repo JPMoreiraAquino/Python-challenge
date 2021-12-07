11d14a22b564d006b258fb5c0001fe49236b44ea
João Pedro Moreira De Aquino <jp02120123@gmail.com>
2021-12-06 22:02:49 -0300

    Vetor

---
+++ exercício27ProgramaQueLerNegativos/Aula01.py
@@ -0,0 +1,20 @@
+n = input("how many numbers will you type?:")
+n = int(n)
+
+negative_numbers = []
+accumulator = 0
+if n > 10:
+    print('impossible to calculate'.upper())
+else:
+    for i in range(n):
+        num = input("typer one number:")
+        num = int(num)
+        if num < 0:
+            negative_numbers.append(num)
+            accumulator += 1
+
+print('negative numbers'.upper())
+for i in range(accumulator):
+    print(negative_numbers[i])
+
+
